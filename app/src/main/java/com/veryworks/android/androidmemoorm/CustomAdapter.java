8c568a8bf6fe0e82e8f98f7cfe343049a9e20497
javafa <nigne2016@gmail.com>
2017-09-22 14:44:56 +0900

    add RecyclerView Adapter

---
+++ app/src/main/java/com/veryworks/android/androidmemoorm/CustomAdapter.java
@@ -0,0 +1,62 @@
+package com.veryworks.android.androidmemoorm;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.veryworks.android.androidmemoorm.model.PicNote;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pc on 9/22/2017.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter{
+    // 1. 데이터 저장소
+    private ArrayList<PicNote> data;
+
+    public void setData(ArrayList<PicNote> data){
+        this.data = data;
+    }
+
+    // 2. 개수
+    @Override
+    public int getItemCount() { // 목록의 전체 길이를 결정
+        return data.size();
+    }
+    // 3. 홀더 생성
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 1. 만들어둔 layout 파일을 inflate 한다
+        View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list, parent, false);
+        // 2. inflate 된 View 를 Holder의 생성자에 담는다
+        Holder holder = new Holder(view);
+        // 3. 생성된 Holder를 리턴한다.
+        return holder;
+    }
+
+    // 4. 홀더 사용
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        // 1. 데이터저장소에 객체단위로 꺼내둔다
+        PicNote picNote = data.get(position);
+        // 2. 홀더에 있는 위젯에 값을 입력한다.
+        ((Holder)holder).setTitle(picNote.getTitle());
+    }
+
+    // 0. 홀더 만들기
+    public class Holder extends RecyclerView.ViewHolder{
+        private TextView textTitle;
+        public Holder(View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+        }
+        public void setTitle(String title){
+            textTitle.setText(title);
+        }
+    }
+}
--- app/src/main/java/com/veryworks/android/androidmemoorm/MainActivity.java
+++ app/src/main/java/com/veryworks/android/androidmemoorm/MainActivity.java
@@ -3,6 +3,22 @@ package com.veryworks.android.androidmemoorm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * RecyclerView 를 사용한 목록 만들기
+ *
+ * 0. 화면만들기
+ *
+ * 1. 데이터를 정의
+ *
+ * 2. 아답터를 재정의
+ *
+ * 3. 재정의한 아답터를 생성하면서 데이터를 담는다
+ *
+ * 4. 아답터와 RecyclerView 컨테이너를 연결
+ *
+ * 5. RecyclerView 에 레이아웃매니저를 성정
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
