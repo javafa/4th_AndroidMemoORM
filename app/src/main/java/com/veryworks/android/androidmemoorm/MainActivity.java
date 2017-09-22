f3c4a62ac2fea88002094bf3224a36852362705a
javafa <nigne2016@gmail.com>
2017-09-22 16:16:00 +0900

    add DrawActivity add capture layout cache

--- app/src/main/java/com/veryworks/android/androidmemoorm/MainActivity.java
+++ app/src/main/java/com/veryworks/android/androidmemoorm/MainActivity.java
@@ -1,9 +1,11 @@
 package com.veryworks.android.androidmemoorm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.veryworks.android.androidmemoorm.dao.PicNoteDAO;
 import com.veryworks.android.androidmemoorm.model.PicNote;
@@ -35,17 +37,26 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    /**
+     * 레이아웃에서 함수를 직접 호출한다
+     * @param view
+     */
+    public void openDraw(View view){
+        Intent intent = new Intent(this, DrawActivity.class);
+        startActivity(intent);
+    }
+
     private void init(){
         PicNoteDAO dao = new PicNoteDAO(this);
         //* 1. 데이터를 정의
         // DB에 테스트 데이터 넣기
-        for(int i=0; i<1000 ; i++){
-            PicNote picNote = new PicNote();
-            picNote.setTitle("안녕하세요 "+i);
-            picNote.setDatetime(System.currentTimeMillis());
-            // db 에다가 넣은후
-            dao.create(picNote);
-        }
+//        for(int i=0; i<1000 ; i++){
+//            PicNote picNote = new PicNote();
+//            picNote.setTitle("안녕하세요 "+i);
+//            picNote.setDatetime(System.currentTimeMillis());
+//            // db 에다가 넣은후
+//            dao.create(picNote);
+//        }
         // db에서 읽어온다.
         List<PicNote> data = dao.readAll();
 
@@ -54,8 +65,10 @@ public class MainActivity extends AppCompatActivity {
         adapter.setData(data);
         //* 4. 아답터와 RecyclerView 컨테이너를 연결
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recylerView);
+        recyclerView.setAdapter(adapter);
         //* 5. RecyclerView 에 레이아웃매니저를 설정
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         // 레이아웃 매니저 종류
         /*
         1. LinearLayoutManager
